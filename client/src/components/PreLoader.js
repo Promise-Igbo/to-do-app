2b5b2ab83fe4afb480db6e1fdea5336cff48c761
reem <janinareem@gmail.com>
2022-09-26 21:42:44 +0200

    Add more crud operation for the to do app

--- client/src/components/PreLoader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PreLoader = () => {
-  return (
-   <h1 className='preloader'>Loading...</h1>
-  )
-}
-
-export default PreLoader
--- server.js
+++ server.js
@@ -1,49 +1,64 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const TaskModel = require('./models/task')
 const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 app.use(cors());
 
-
 // middleware for data on post request
-app.use(express.json({extended: false}))
+app.use(express.json())
 
-const todos = [
-  {
-    message: 'call mom',
-    id: 1
-  },
-  {
-    message: "eat chiken",
-    id: 2
-  },
-  {
-    message: "go to the coffe shop",
-    id: 3
-  }
-]
 
-app.get('/', (req, res) => {
-    res.status(200).json(todos)
+/// DATABASE CONNECTION
+mongoose.connect(
+  "mongodb://localhost:27017/TodoList?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+  { useNewUrlParser: true }
+);
+
+
+app.post("/addTask", async (req, res) => {
+   const task = req.body.task;
+
+  const newTask = new TaskModel({ task: task });
+  await newTask.save();
+  res.send(newTask);
+});
+
+app.put("/update", async (req, res) => {
+    const newTask = req.body.newTask;
+    const id = req.body.id;
+
+    try {
+       await TaskModel.findById(id, (error, taskToUpdate) => {
+          taskToUpdate.task = newTask;
+          taskToUpdate.save();
+       })
+    } catch (err) {
+      console.log(err)
+    }
+
+    res.send("updated1")
+
 })
 
-app.post('/', (req, res) => {
+app.get("/read", async (req, res) => {
+  TaskModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
-  const newTodo = {
-  message: req.body.message,
-  id: uuidv4()
-  }
 
-  todos.push(newTodo);
-  res.status(201).json(todos)
-})
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
 
-app.delete('/:id', (req, res) => {
-  const id = req.body.id;
-
-  todos.findByIdAndDelete(id)
-  res.status(200).json(todos)
+  await TaskModel.findByIdAndRemove(id);
+  res.send("item deleted!");
 })
 
 const PORT = process.env.PORT || 8080;
@@ -52,3 +67,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
      console.log(`server running on: ${PORT}`)
 })
+
