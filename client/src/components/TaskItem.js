05796017f7b47612ffd083e3d1bbec8613cd96a5
reem <janinareem@gmail.com>
2022-10-13 22:54:38 +0200

    Add check on the list

--- client/src/components/TaskItem.js
+++ client/src/components/TaskItem.js
@@ -1,7 +1,12 @@
-import { useState, useEffect } from 'react';
+import {
+  useState,
+  useEffect
+} from 'react';
 import Axios from 'axios';
 import './TaskItem.css';
 import Header from './Header';
+import { ToastContainer, toast }from 'react-toastify';
+
 
 
 function TaskItem() {
@@ -9,96 +14,142 @@ function TaskItem() {
   const [listOfTasks, setListOfTask] = useState([]);
 
 
+  const generateError = err => toast.error(err, {
+    position:"bottom-right"
+ });
+
   const addTask = () => {
-      Axios.post("http://localhost:8080/addTask",
-       { task: task }, {
+    let flag = true;
+
+    listOfTasks.forEach(t => {
+      if (t.task === task) flag = false;
+      return;
+    })
+
+    if (!flag) { 
+      generateError("Task found please enter a new task") ;
+      return
+    }
+
+    Axios.post("http://localhost:8080/addTask", {
+        task: task
+      }, {
         withCredentials: true,
-       })
-       .then(() => {
-        setListOfTask([...listOfTasks, { task: task}])
-       })
-      setTask('');
+      })
+      .then(() => {
+        setListOfTask([...listOfTasks, {
+          task: task
+        }])
+      })
+
+    setTask('');
   };
 
 
   const updateTask = (id) => {
-      const newTask = prompt("Enter new task");
+    const newTask = prompt("Enter new task");
 
-      Axios.put("http://localhost:8080/update", { newTask: newTask, id: id}, {
-        withCredentials: true,
-       }).then(() => {
-        setListOfTask(listOfTasks.map((val) => {
-           return val._id === id ? { _id: id, task: newTask} : val;
-        }))
-      })
+    Axios.put("http://localhost:8080/update", {
+      newTask: newTask,
+      id: id
+    }, {
+      withCredentials: true,
+    }).then(() => {
+      setListOfTask(listOfTasks.map((val) => {
+        return val._id === id ? {
+          _id: id,
+          task: newTask
+        } : val;
+      }))
+    })
   }
 
   const deleteTask = (id) => {
     Axios.delete(`http://localhost:8080/delete/${id}`, {
       withCredentials: true,
-     }).then(() => {
-        setListOfTask(
-          listOfTasks.filter((val) => {
-            return val._id !== id;
-          })
-        )
+    }).then(() => {
+      setListOfTask(
+        listOfTasks.filter((val) => {
+          return val._id !== id;
+        })
+      )
     })
   }
 
   useEffect(() => {
     Axios.get("http://localhost:8080/read", {
-      withCredentials: true,
-     })
-    .then((response)  => {
-     setListOfTask(response.data)
-   })
-   .catch(() => {
-     console.log("Err");
-   });
-  }, [setListOfTask, listOfTasks]);
+        withCredentials: true,
+      })
+      .then((response) => {
+        setListOfTask(response.data)
+      })
+      .catch(() => {
+        console.log("Err");
+      });
+  }, [listOfTasks]);
 
 
 
-  return (
-    <div className="App">
-      <Header />
-      <div className='container'>
-            <input 
-            className='input'
-            type="text" 
-            value={task}
-            placeholder='Add your to do task...'
-            onChange={(e) => {
-              setTask(e.target.value)
-            }}
-            />
-      <button className='submitbtn' onClick={addTask}>Add task</button>
-
-      { listOfTasks.length > 0 && listOfTasks.map((val) => (
-            <div key={val._id} id={val._id} className='todoitem'>
-                 <h1>{val.task}</h1>
-                 <button 
-                 className='updatebtn'
-                 onClick={()=> {
-                    updateTask(val._id);
-                 }}
-                 >
-                  Update
-                 </button>
-                 <button 
-                 className='removebtn'
-                 onClick={()=> { 
-                  deleteTask(val._id) 
-                }}
-                 >
-                  Remove
-                  </button>
-           </div>
-              ))
+  return ( <
+    div className = "App" >
+    <
+    Header / >
+    <
+    div className = 'container' >
+    <
+    input className = 'input'
+    type = "text"
+    value = {
+      task
+    }
+    placeholder = 'Add your to do task...'
+    onChange = {
+      (e) => {
+        setTask(e.target.value)
       }
+    }
+    /> <
+    button className = 'submitbtn'
+    onClick = {
+      addTask
+    } > Add task < /button>
 
-      </div>
-    </div>
+    {
+      listOfTasks.length > 0 && listOfTasks.map((val) => ( <
+        div key = {
+          val._id
+        }
+        id = {
+          val._id
+        }
+        className = 'todoitem' >
+        <
+        h1 > {
+          val.task
+        } < /h1> <
+        button className = 'updatebtn'
+        onClick = {
+          () => {
+            updateTask(val._id);
+          }
+        } >
+        Update <
+        /button> <
+        button className = 'removebtn'
+        onClick = {
+          () => {
+            deleteTask(val._id)
+          }
+        } >
+        Remove <
+        /button> < /
+        div >
+      ))
+    }
+
+    <
+    /div> < /
+    div >
   );
 }
 
